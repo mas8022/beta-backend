c30beb868b221fe3aabc4f0e1002d26ea1e2dc32
masproo22 <m.abbaszadeh2002@gmail.com>
2025-09-15 11:30:38 +0330

    fix: contact-us

--- src/modules/http-api/contact-us/contact-us.service.ts
+++ src/modules/http-api/contact-us/contact-us.service.ts
@@ -7,11 +7,8 @@ export class ContactUsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createContactUsDto: CreateContactUsDto) {
-    try {
-      await this.prismaService.contactUs.create({ data: createContactUsDto });
-      return { status: 201, messages: 'پیام ارسال شد' };
-    } catch (error) {
-      return { status: 500, messages: 'اینترنت خود را بررسی کنید' };
-    }
+    await this.prismaService.contactUs.create({ data: createContactUsDto });
+
+    return { status: 201, message: 'پیام ارسال شد' };
   }
 }
