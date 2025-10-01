41dfaaec437d88937d7848bb6cbe9d55de198242
masproo22 <m.abbaszadeh2002@gmail.com>
2025-10-01 18:02:25 +0330

    feat: api for /admin-panel/courses/[id] page

--- src/modules/http-api/admins/admins.controller.ts
+++ src/modules/http-api/admins/admins.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminsService } from './admins.service';
 import { GetCoursesDto } from './dto/get-courses.dto';
 import { AdminGuard } from './admin.guard';
@@ -11,5 +20,53 @@ export class AdminsController {
   @Get('courses')
   async getCourses(@Query() query: GetCoursesDto) {
     return await this.adminsService.getCourses(query);
+  }
+
+  @Patch('reject-course/:id')
+  async rejectCourse(@Param('id') id: string) {
+    return await this.adminsService.rejectCourse(id);
+  }
+
+  @Patch('accepte-course/:id')
+  async accepteCourse(@Param('id') id: string) {
+    return await this.adminsService.accepteCourse(id);
+  }
+
+  @Post('send-course-report/:id')
+  async sendCourseReport(
+    @Param('id') id: string,
+    @Body('message') message: string,
+  ) {
+    return await this.adminsService.sendCourseReport(id, message);
+  }
+
+  @Get('over-view-course/:id')
+  async getOverViewCourse(@Param('id') id: string) {
+    return this.adminsService.getOverViewCourse(id);
+  }
+
+  @Get('course-lessons/:id')
+  async getCourseLessons(@Param('id') id: string) {
+    return this.adminsService.getCourseLessons(id);
+  }
+
+  @Patch('reject-episode/:id')
+  async rejectEpisode(@Param('id') id: string) {
+    return await this.adminsService.rejectEpisode(id);
+  }
+
+  @Patch('accepte-episode/:id')
+  async accepteEpisode(@Param('id') id: string) {
+    return await this.adminsService.accepteEpisode(id);
+  }
+
+  @Patch('reject-lesson/:lessonId')
+  async rejectLesson(@Param('lessonId') lessonId: string) {
+    return await this.adminsService.rejectLesson(lessonId);
+  }
+
+  @Patch('accepte-lesson/:lessonId')
+  async accepteLesson(@Param('lessonId') lessonId: string) {
+    return await this.adminsService.accepteLesson(lessonId);
   }
 }
--- src/modules/http-api/admins/admins.service.ts
+++ src/modules/http-api/admins/admins.service.ts
@@ -65,4 +65,162 @@ export class AdminsService {
 
     return { status: 200, data: courses };
   }
+
+  async rejectCourse(courseId: string) {
+    await this.prismaService.course.update({
+      where: {
+        id: Number(courseId),
+      },
+      data: {
+        status: 'rejected',
+      },
+    });
+
+    return { status: 201, message: 'دوره رد شد' };
+  }
+
+  async accepteCourse(courseId: string) {
+    await this.prismaService.course.update({
+      where: {
+        id: Number(courseId),
+      },
+      data: {
+        status: 'publish',
+      },
+    });
+
+    return { status: 201, message: 'دوره پذیرفته شد' };
+  }
+
+  async sendCourseReport(courseId: string, message: string) {
+    const course = await this.prismaService.course.findUnique({
+      where: {
+        id: Number(courseId),
+      },
+      select: {
+        author: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    const authorId = course?.author.id;
+
+    if (!authorId) {
+      return { status: 201, message: 'نویسنده این دوره اخراج شده' };
+    }
+
+    await this.prismaService.courseReport.create({
+      data: {
+        message,
+        authorId,
+        courseId: Number(courseId),
+      },
+    });
+
+    return { status: 201, message: 'پیام ارسال شد' };
+  }
+
+  async getOverViewCourse(courseId: string) {
+    const cousre = await this.prismaService.course.findUnique({
+      where: {
+        id: Number(courseId),
+      },
+
+      select: {
+        id: true,
+        image: true,
+        title: true,
+        category: true,
+        description: true,
+        price: true,
+        originalPrice: true,
+        requirements: true,
+        whatYouLearn: true,
+      },
+    });
+
+    return { status: 200, data: cousre };
+  }
+
+  async getCourseLessons(courseId: string) {
+    const lessons = await this.prismaService.course.findUnique({
+      where: {
+        id: Number(courseId),
+      },
+
+      select: {
+        id: true,
+        status: true,
+        lessons: {
+          include: {
+            episodes: {
+              orderBy: {
+                order: 'asc',
+              },
+            },
+          },
+          orderBy: {
+            order: 'asc',
+          },
+        },
+      },
+    });
+
+    return { status: 200, data: lessons };
+  }
+
+  async rejectEpisode(id: string) {
+    await this.prismaService.episode.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        status: 'rejected',
+      },
+    });
+
+    return { status: 201, message: 'درس رد شد' };
+  }
+
+  async accepteEpisode(id: string) {
+    await this.prismaService.episode.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        status: 'publish',
+      },
+    });
+
+    return { status: 201, message: 'درس پذیرفته شد' };
+  }
+
+  async rejectLesson(lessonId: string) {
+    await this.prismaService.lesson.update({
+      where: {
+        id: Number(lessonId),
+      },
+      data: {
+        status: 'rejected',
+      },
+    });
+
+    return { status: 201, message: 'فصل رد شد' };
+  }
+
+  async accepteLesson(lessonId: string) {
+    await this.prismaService.lesson.update({
+      where: {
+        id: Number(lessonId),
+      },
+      data: {
+        status: 'publish',
+      },
+    });
+
+    return { status: 201, message: 'فصل پذیرفته شد' };
+  }
 }
