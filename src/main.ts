6a6bb4452438af2a296c596749c0002b57ccd451
masproo22 <m.abbaszadeh2002@gmail.com>
2025-11-04 19:15:16 +0330

    fix: the security in main.ts with add helmet package and add trustProxy:true to show real users ip

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ import {
 } from '@nestjs/platform-fastify';
 import fastifyCookie from '@fastify/cookie';
 import multipart from '@fastify/multipart';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const fastifyAdapter = new FastifyAdapter();
+  const fastifyAdapter = new FastifyAdapter({ trustProxy: true });
 
   await fastifyAdapter.register(fastifyCookie, {
     secret: process.env.FASTIFY_COOKIE_SECRET,
   });
-
   await fastifyAdapter.register(multipart);
+
+  await fastifyAdapter.register(helmet);
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
